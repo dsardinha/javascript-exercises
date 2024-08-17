8a505f8a075f798e7b0e3741d86175d2d81caa2d
dsardinha <15276275+dsardinha@users.noreply.github.com>
2024-08-17 17:54:46 +0100

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num !== 'number') num = +num;
+    if (num < 0) return "OOPS";
+    if (num === 0 || isNaN(num)) return 0;
+    if (num === 1 || num === 2) return 1;
+    let arr = [1, 1];
+    for (let i = 1; i < num; i++) {
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    }
+    return arr[num - 1];
 };
 
 // Do not edit below this line
